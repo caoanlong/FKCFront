c48cc85b540497ed282a7cf149f77bbf822ea5e5
caoanlong <476977540@qq.com>
2017-11-13 08:07:27 +0800

    update

--- src/components/AccountDetail.vue
+++ src/components/AccountDetail.vue
@@ -1,43 +1,62 @@
 <template>
-  	<div class="account">
-      <group title="金豆明细记录（近三月）">
-        <cell v-for="(item,i) in 7" :key="i">
-          <div slot="title">
-            <p>全民踩踩踩</p>
-            <p style="font-size: 14px;color: #999">9月13日 21:30</p>
-          </div>
-          <div slot="default">
-            <span>+68</span>
-          </div>
-        </cell>
-      </group>
-    </div>
+		<div class="account">
+			<group title="金豆明细记录（近三月）">
+				<cell v-for="(item,i) in accountDetails" :key="i">
+					<div slot="title">
+						<p class="eclipsis" :style="{width: winWidth}">{{item.type}}<span style="color:#666;font-size:16px">({{item.info}})</span></p>
+						<p style="font-size: 14px;color: #999">9月13日 21:30</p>
+					</div>
+					<div slot="default">
+						<span>{{item.goldBeanChange}}</span>
+					</div>
+				</cell>
+			</group>
+		</div>
 </template>
 <script>
 import { Group,Cell } from 'vux'
 export default {
-  	data () {
-    	return {
-    	}
-  	},
-  	created() {
-  		this.$store.commit({
-  			type: 'changeTitle',
-  			title: '账户明细',
-  			isCome: true,
-  			isAdd: false,
-  		})
-  	},
-    components: {
-      Group,
-      Cell
-    }
+		data () {
+			return {
+				accountDetails: []
+			}
+		},
+		computed: {
+			winWidth() {
+				console.log(window.screen)
+				return (window.screen.width-90) + 'px'
+			}
+		},
+		created() {
+			this.$store.commit({
+				type: 'changeTitle',
+				title: '账户明细',
+				isCome: true,
+				isAdd: false,
+			})
+			this.getAccountDetail()
+		},
+		methods: {
+			getAccountDetail() {
+				let URL = this.__WEBSERVERURL__ + '/api/member/accountDetails';
+	  			this.$http.post(URL).then((res) => {
+	  				if (res.body.code == 0) {
+	  					this.accountDetails = res.body.data
+	  				}
+	  				console.log(JSON.stringify(res.body.data));
+	  			})
+			}
+		},
+		components: {
+			Group,
+			Cell
+		}
 }
 </script>
 <style lang="stylus" rel="stylesheet/stylus" scoped>
-  .account
-    width 100%
-    position absolute
-    top 46px
-    left 0
+	.account
+		width 100%
+		position absolute
+		top 46px
+		left 0
 </style>
--- src/components/My.vue
+++ src/components/My.vue
@@ -1,67 +1,91 @@
 <template>
 	<div class="my">
-	  	<div class="profile">
-	  		<div class="head">
-	  			<img src="../assets/img/avatar.svg">
-	  		</div>
-	  		<div class="mobile">
-	  			<p>{{memberInfo.mobile}}</p>
-	  		</div>
-	  	</div>
-	  	<group gutter="0">
-	    	<cell title="我的金豆" :value="memberInfo.goldBean" is-link :link="{name: 'getGoldBean'}"></cell>
-	  	</group>
-	  	<group>
-	    	<cell title="我的竞猜" is-link :link="{name: 'myGuess'}"></cell>
-	    	<cell title="账户明细" is-link :link="{name: 'accountDetail'}"></cell>
-	  	</group>
-	  	<group>
-	    	<cell title="分享给好友" is-link link=""></cell>
-	  	</group>
-	  	<box gap="20px 10px">
-	    	<x-button type="warn" @click.native="signout">退出</x-button>
-	  	</box>
-  	</div>
+		<div class="profile">
+			<div class="head">
+				<input type="file" class="avatar" ref="avatar" @change="addImg">
+				<img :src="__WEBIMGSERVERURL__ + memberInfo.avatar" v-if="memberInfo.avatar">
+				<img src="../assets/img/avatar.svg" v-else>
+			</div>
+			<div class="mobile">
+				<p>{{memberInfo.mobile}}</p>
+			</div>
+		</div>
+		<group gutter="0">
+			<cell title="我的金豆" :value="memberInfo.goldBean" is-link :link="{name: 'getGoldBean'}"></cell>
+		</group>
+		<group>
+			<cell title="我的竞猜" is-link :link="{name: 'myGuess'}"></cell>
+			<cell title="账户明细" is-link :link="{name: 'accountDetail'}"></cell>
+		</group>
+		<group>
+			<cell title="分享给好友" is-link link=""></cell>
+		</group>
+		<box gap="20px 10px">
+			<x-button type="warn" @click.native="signout">退出</x-button>
+		</box>
+	</div>
 </template>
 <script>
 	import { Group,Cell,XButton,Box } from 'vux'
 	export default {
 		data () {
-		    return {
-		    	memberInfo: {}
-		    }
+			return {
+				memberInfo: {}
+			}
 		},
 		created() {
-	  		this.$store.commit({
-	  			type: 'changeTitle',
-	  			title: '我的',
-	  			isCome: true,
-	  			isAdd: false,
-	  		})
-	  		this.getMemberInfo()
-	  	},
-	  	methods: {
-	  		getMemberInfo() {
-	  			let URL = this.__WEBSERVERURL__ + '/api/member/info';
-	  			this.$http.post(URL).then((res) => {
-	  				if (res.body.code == 0) {
-	  					this.memberInfo = res.body.data
-	  					localStorage.setItem('memberInfo',JSON.stringify(res.body.data))
-	  				}
-	  				console.log(JSON.stringify(res.body.data));
-	  			})
-	  		},
-	  		signout() {
-	  			localStorage.clear();
-	  			this.$router.push({name: 'login'})
-	  		}
-	  	},
-	  	components: {
-	  		Group,
-	  		Cell,
-	  		XButton,
-	  		Box
-	  	}
+			this.$store.commit({
+				type: 'changeTitle',
+				title: '我的',
+				isCome: true,
+				isAdd: false,
+			})
+			this.getMemberInfo()
+		},
+		methods: {
+			getMemberInfo() {
+				let URL = this.__WEBSERVERURL__ + '/api/member/info';
+				this.$http.post(URL).then((res) => {
+					if (res.body.code == 0) {
+						this.memberInfo = res.body.data
+						localStorage.setItem('memberInfo',JSON.stringify(res.body.data))
+					}
+					console.log(JSON.stringify(res.body.data));
+				})
+			},
+			addImg(e) {
+				let URL = this.__WEBSERVERURL__ + '/uploadImg';
+				let formData = new FormData()
+				formData.append("file",e.target.files[0])
+				this.$http.post(URL,formData).then((res) => {
+					this.uploadAvatar(res.body.data)
+				})
+			},
+			uploadAvatar(imgUrl) {
+				let URL = this.__WEBSERVERURL__ + '/api/member/avatar';
+				let params = {
+					memberId: this.memberInfo._id,
+					avatar: imgUrl
+				}
+				this.$http.post(URL,params).then((res) => {
+					if (res.body.code == 0) {
+						this.memberInfo = res.body.data
+						localStorage.setItem('memberInfo',JSON.stringify(res.body.data))
+					}
+					console.log(JSON.stringify(res.body.data));
+				})
+			},
+			signout() {
+				localStorage.clear();
+				this.$router.push({name: 'login'})
+			}
+		},
+		components: {
+			Group,
+			Cell,
+			XButton,
+			Box
+		}
 	}
 </script>
 <style lang="stylus" rel="stylesheet/stylus" scoped>
@@ -80,8 +104,17 @@
 				height 80px
 				border-radius 40px
 				background-color #fff
-				padding 1px
+				border 2px solid #fff
 				margin 0 auto
+				overflow hidden
+				position relative
+				.avatar
+					position absolute
+					width 100%
+					height 100%
+					left 0
+					top 0
+					opacity 0
 				img
 					display block
 					width 100%
