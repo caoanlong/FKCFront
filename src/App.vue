89fa458a87754dbb2da012f02b749e0fdbebc5c2
caoanlong <476977540@qq.com>
2017-12-21 07:52:42 +0800

    aaa

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,64 @@
 <template>
-  	<div class="container">
-    	<router-view></router-view>
-  	</div>
+	<div class="container">
+		<router-view></router-view>
+	</div>
 </template>
 <script>
 	export default {
 		data () {
 			return {
+				// 分享
+				title: '91疯狂猜',
+				desc: '猜体育、经济、娱乐事件、猜猜更精彩！'
+			}
+		},
+		watch: {
+			$route () {
+				// 微信分享
+				this.getWeixinConfig(location.href, () => {
+					wx.onMenuShareTimeline({
+						title: this.title, // 分享标题
+						desc: this.desc, // 分享描述
+						link: location.origin, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+						imgUrl: this.__WEBIMGSERVERURL__ + '/file-1513780566518.jpeg', // 分享图标
+						success: function () {
+						// 用户确认分享后执行的回调函数
+						},
+						cancel: function () {
+						// 用户取消分享后执行的回调函数
+						}
+					})
+					wx.onMenuShareAppMessage({
+						title: this.title, // 分享标题
+						desc: this.desc, // 分享描述
+						link: location.origin, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+						imgUrl: this.__WEBIMGSERVERURL__ + '/file-1513780566518.jpeg', // 分享图标
+						type: '', // 分享类型,music、video或link，不填默认为link
+						dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
+						success: function () {
+						// 用户确认分享后执行的回调函数
+						},
+						cancel: function () {
+						// 用户取消分享后执行的回调函数
+						}
+					})
+					wx.onMenuShareQQ({
+						title: this.title, // 分享标题
+						desc: this.desc, // 分享描述
+						link: location.origin, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+						imgUrl: this.__WEBIMGSERVERURL__ + '/file-1513780566518.jpeg', // 分享图标
+						success: function () {
+						// 用户确认分享后执行的回调函数
+						},
+						cancel: function () {
+						// 用户取消分享后执行的回调函数
+						}
+					})
+				})
 			}
 		},
 		created () {
+			
 		},
 		methods: {
 		}
--- src/assets/js/common.js
+++ src/assets/js/common.js
@@ -38,4 +38,46 @@ export default function install (Vue, option) {
 			return year + "-" + month + "-" + date + " " + hour + ":" + minute + ":" + second
 		}
 	}
+	Vue.prototype.getWeixinConfig = function (url, callback, bool) {
+		let URL = this.__WEBSERVERURL__ + '/weixin/config'
+		let params = {
+			url: url
+		}
+		Vue.http.post(URL, params).then(res => {
+			let timestamp = res.body.data.timestamp
+			let nonceStr = res.body.data.noncestr
+			let signature = res.body.data.signature
+			wx.config({
+				// debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+				appId: 'wxe0fd26ab323ce46d', // 必填，公众号的唯一标识
+				timestamp: timestamp, // 必填，生成签名的时间戳
+				nonceStr: nonceStr, // 必填，生成签名的随机串
+				signature: signature,// 必填，签名，见附录1
+				jsApiList: [
+					'onMenuShareTimeline',
+					'onMenuShareAppMessage',
+					'onMenuShareQQ'
+				] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+			})
+			wx.ready(function(){
+				if (bool) {
+					callback()
+				}
+			})
+			wx.error(function(res){
+				console.log(res)
+			})
+			wx.checkJsApi({
+				jsApiList: ['chooseImage'], // 需要检测的JS接口列表，所有JS接口列表见附录2,
+				success: function(res) {
+					console.log(res)
+				// 以键值对的形式返回，可用的api值true，不可用为false
+				// 如：{"checkResult":{"chooseImage":true},"errMsg":"checkJsApi:ok"}
+				}
+			})
+			if (!bool) {
+				callback()
+			}
+		})
+	}
 }
