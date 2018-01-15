b9675f6140d4922637a5a7f967e57a0d7a825f9f
Caoanlong <caoanlong@gmail.com>
2018-01-15 09:22:19 +0800

    gg

--- src/App.vue
+++ src/App.vue
@@ -58,7 +58,7 @@
 					wx.onMenuShareTimeline({
 						title: this.title + '，' + this.desc, // 分享标题
 						desc: this.desc, // 分享描述
-						link: this.shareLink, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+						link: location.href, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
 						imgUrl: this.__WEBIMGSERVERURL__ + '/file-1513780566518.jpeg', // 分享图标
 						success: function () {
 						// 用户确认分享后执行的回调函数
@@ -71,7 +71,7 @@
 					wx.onMenuShareAppMessage({
 						title: this.title, // 分享标题
 						desc: this.desc, // 分享描述
-						link: this.shareLink, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+						link: location.href, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
 						imgUrl: this.__WEBIMGSERVERURL__ + '/file-1513780566518.jpeg', // 分享图标
 						type: '', // 分享类型,music、video或link，不填默认为link
 						dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
@@ -95,7 +95,7 @@
 						// 用户取消分享后执行的回调函数
 						}
 					})
-				})
+				}, true)
 			}
 		}
 	}
