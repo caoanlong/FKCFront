dd1edfc09d5fc435a5dfb6457a1b4a8f2c0daa01
caoanlong <476977540@qq.com>
2018-01-26 08:31:21 +0800

    修改为原来的支付

--- src/assets/js/common.js
+++ src/assets/js/common.js
@@ -18,6 +18,7 @@ function getStyle(obj,attr){
 
 export default function install (Vue, option) {
 	// Vue.prototype.__WEBSERVERURL__ = 'http://127.0.0.1:3000'
+	// Vue.prototype.__WEBIMGSERVERURL__ = 'http://127.0.0.1:4000'
 	Vue.prototype.__WEBSERVERURL__ = 'http://39.108.245.177:3000'
 	Vue.prototype.__WEBIMGSERVERURL__ = 'http://39.108.245.177:4000'
 	Vue.prototype.getVerCode = function (num) {
